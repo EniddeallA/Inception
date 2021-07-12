f93538a627e019ab93d9144ed9114884f46bb81a
eniddealla <allaeddinekhalid@gmail.com>
2021-07-12 17:03:46 +0100

    progress

--- srcs/requirements/mariadb/start.sh
+++ srcs/requirements/mariadb/start.sh
@@ -1,6 +1,10 @@
+sed -i "s/127\.0\.0\.1/0\.0\.0\.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+
 service mysql start
 
-mysql -u root -e "CREATE USER 'wp-user'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'"
-mysql -u root -e "CREATE DATABASE wordpress"
-mysql -u root -e "USE wordpress; GRANT ALL PRIVILEGES ON * TO 'wp-user'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES"
+mysql -u root -e "CREATE USER '${dbuser}'@'%' IDENTIFIED BY '${userpw}'"
+mysql -u root -e "CREATE DATABASE wordpress;"
+mysql -u root -e "USE wordpress;GRANT ALL PRIVILEGES ON * TO '${dbuser}'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${rootpw}';"
+
 mysqld_safe
--- srcs/requirements/wordpress/wp-config.php
+++ srcs/requirements/wordpress/wp-config.php
@@ -23,13 +23,13 @@
 define( 'DB_NAME', 'wordpress' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'akhalid' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'password' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'mariadb' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
@@ -77,7 +77,7 @@ $table_prefix = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', False);
 
 /* That's all, stop editing! Happy publishing. */
 
