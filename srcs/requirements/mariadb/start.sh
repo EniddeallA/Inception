747d663b23398cd5a3b6c20d111fde2867549fcb
eniddealla <allaeddinekhalid@gmail.com>
2021-07-12 20:39:57 +0100

    progress

--- srcs/requirements/mariadb/start.sh
+++ srcs/requirements/mariadb/start.sh
@@ -5,7 +5,7 @@ service mysql start
 mysql -u root -e "CREATE USER '${dbuser}'@'%' IDENTIFIED BY '${userpw}'"
 mysql -u root -e "CREATE DATABASE wordpress;"
 mysql -u root -e "USE wordpress;GRANT ALL PRIVILEGES ON * TO '${dbuser}'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-mysql -u root wordpress < /wp.sql
+mysql -u root wordpress < wp.sql
 mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${rootpw}';"
 
 mysqld_safe
